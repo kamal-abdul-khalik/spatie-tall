c22e4a5f64bd44f09dea96f4534d22d83168d398
kamal-abdul-khalik <elsakcf@gmail.com>
2024-01-02 03:39:50 +0800

    wip sass

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,17 +1,143 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+        <h2 class="text-xl font-semibold leading-tight text-gray-800 dark:text-gray-200">
             {{ __('Dashboard') }}
         </h2>
     </x-slot>
 
     <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __("You're logged in!") }}
+        <div class="container">
+            <div class="card">
+                <div class="card-title">{{ __("You're logged in!") }}</div>
+                <div class="card-body">{{ __("You're logged in!") }}</div>
+            </div>
+            <h1>Hello Sass h1</h1>
+            <h2>Hello Sass h2</h2>
+            <h3>Hello Sass h3</h3>
+            <h4>Hello Sass h4</h4>
+            <h5>Hello Sass h5</h5>
+            <h6>Hello Sass h6</h6>
+            <div class="mt-6"></div>
+            <div>
+                <button class="btn btn-primary">
+                    Primary
+                </button>
+                <button class="btn btn-secondary">
+                    Secondary
+                </button>
+                <button class="btn btn-danger">
+                    Danger
+                </button>
+                <button class="btn btn-success">
+                    Success
+                </button>
+                <button class="btn btn-warning">
+                    Warning
+                </button>
+                <button class="btn btn-info">
+                    Info
+                </button>
+                <button class="btn btn-disabled">
+                    Disabled
+                </button>
+            </div>
+
+            <div class="mt-6"></div>
+            <div>
+                <div class="card">
+                    <div class="card-title">
+                        dfs
+                    </div>
+                    <div class="card-body">
+                        sas
+                    </div>
+                </div>
+                <div class="mt-6"></div>
+                <div class="card card-primary">
+                    <div class="card-title">Abcd</div>
+                    <div class="card-body">
+                        sas
+                    </div>
+                </div>
+                <div class="mt-6"></div>
+                <div class="card card-secondary">
+                    <div class="card-title">Abcd</div>
+                    <div class="card-body">
+                        sas
+                    </div>
+                </div>
+                <div class="mt-6"></div>
+                <div class="card card-success">
+                    <div class="card-title">Abcd</div>
+                    <div class="card-body">
+                        sas
+                    </div>
+                </div>
+                <div class="mt-6"></div>
+                <div class="card card-danger">
+                    <div class="card-title">Abcd</div>
+                    <div class="card-body">
+                        sas
+                    </div>
+                </div>
+                <div class="mt-6"></div>
+                <div class="card card-info">
+                    <div class="card-title">Abcd</div>
+                    <div class="card-body">
+                        sas
+                    </div>
                 </div>
             </div>
+            <div class="mt-6"></div>
+            <div>
+                <div class="alert alert-primary">
+                    primary
+                </div>
+                <div class="alert alert-danger">
+                    danger
+                </div>
+                <div class="alert alert-success">
+                    success
+                </div>
+                <div class="alert alert-warning">
+                    warning
+                </div>
+                <div class="alert alert-info">
+                    info
+                </div>
+
+                <span class="badge badge-primary">
+                    primary
+                </span>
+                <span class="badge badge-secondary">
+                    secondary
+                </span>
+                <span class="badge badge-danger">
+                    danger
+                </span>
+                <span class="badge badge-success">
+                    success
+                </span>
+                <span class="badge badge-warning">
+                    warning
+                </span>
+                <span class="badge badge-info">
+                    info
+                </span>
+                <span class="badge badge-disabled">
+                    disabled
+                </span>
+            </div>
+            <div class="mt-6"></div>
+
+
         </div>
+
     </div>
+
+
+
+
+
+
 </x-app-layout>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,98 @@
-import defaultTheme from 'tailwindcss/defaultTheme';
-import forms from '@tailwindcss/forms';
+import defaultTheme from "tailwindcss/defaultTheme";
+import forms from "@tailwindcss/forms";
 
 /** @type {import('tailwindcss').Config} */
 export default {
     content: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/views/**/*.blade.php',
+        "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
+        "./storage/framework/views/*.php",
+        "./resources/views/**/*.blade.php",
     ],
 
     theme: {
         extend: {
+            colors: {
+                primary: {
+                    50: "#eef2ff",
+                    100: "#e0e7ff",
+                    200: "#c7d2fe",
+                    300: "#a5b4fc",
+                    400: "#818cf8",
+                    500: "#6366f1",
+                    600: "#4f46e5",
+                    700: "#4338ca",
+                    800: "#3730a3",
+                    900: "#312e81",
+                    950: "#1e1b4b",
+                },
+                secondary: {
+                    300: "#cbd5e1",
+                    400: "#94a3b8",
+                    500: "#64748b",
+                    600: "#475569",
+                    700: "#334155",
+                    800: "#1e293b",
+                    900: "#0f172a",
+                    950: "#020617",
+                },
+                success: {
+                    100: "#dcfce7",
+                    200: "#bbf7d0",
+                    300: "#86efac",
+                    400: "#4ade80",
+                    500: "#22c55e",
+                    600: "#16a34a",
+                    700: "#15803d",
+                    800: "#166534",
+                    900: "#14532d",
+                    950: "#052e16",
+                },
+                danger: {
+                    100: "#ffe4e6",
+                    200: "#fecdd3",
+                    300: "#fda4af",
+                    400: "#fb7185",
+                    500: "#f43f5e",
+                    600: "#e11d48",
+                    700: "#be123c",
+                    800: "#9f1239",
+                    900: "#881337",
+                    950: "#4c0519",
+                },
+                warning: {
+                    100: "#ffedd5",
+                    200: "#fed7aa",
+                    300: "#fdba74",
+                    400: "#fb923c",
+                    500: "#f97316",
+                    600: "#ea580c",
+                    700: "#c2410c",
+                    800: "#9a3412",
+                    900: "#7c2d12",
+                    950: "#431407",
+                },
+                info: {
+                    100: "#e0f2fe",
+                    200: "#bae6fd",
+                    300: "#7dd3fc",
+                    400: "#38bdf8",
+                    500: "#0ea5e9",
+                    600: "#0284c7",
+                    700: "#0369a1",
+                    800: "#075985",
+                    900: "#0c4a6e",
+                    950: "#082f49",
+                },
+                dark: {
+                    1: "#475569",
+                    2: "#334155",
+                    3: "#1e293b",
+                    4: "#0f172a",
+                    5: "#020617",
+                },
+            },
             fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+                sans: ["Figtree", ...defaultTheme.fontFamily.sans],
             },
         },
     },
